78de599060a09171cb3dd4c0d706c061bad17c55
Markus-the-Unwise <sky6308@gmail.com>
2025-05-20 17:01:52 +0800

    Add operate function

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ function add(a, b) { return a + b; };
 function subtract(a, b) { return a - b; };
 function multiply(a, b) { return a * b; };
 function divide(a, b) { return a / b; };
-
+function operate(a, b, opMethod) {
+    let result = 0;
+    switch (opMethod) {
+        case 'add':
+            result = add(a, b);
+            break;
+        case 'subtract':
+            result = subtract(a, b);
+            break
+        case 'multiply':
+            result = multiply(a, b);
+            break
+        case 'divide':
+            result = divide(a, b)
+            break
+        default:
+            break;
+    }
+}
 // Initial values
 let firstValue = 0;
 let secondValue = 0;
-let operation; // 0123 -> +-*/
+let operation;
 
 const status = [false, false, false]; //[FirstValueExists,SecondValueExists,OperationMethodSelected]
 
